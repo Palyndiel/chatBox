54d0167224fcca50e38c5c5b8f834b0bfbe07c6a
maxym.mino@gmail.com <palyndiel@macbook-pro-de-maxime.home>
2018-10-01 18:33:13 +0200

    Ajout de l'application

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCfWgD6MdVspjj2cieeH0rVlRLR_Ns9WRw",
+    authDomain: "chatbox-38d39.firebaseapp.com",
+    databaseURL: "https://chatbox-38d39.firebaseio.com",
+});
+
+export default base;
